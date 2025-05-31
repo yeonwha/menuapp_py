2d6c6d2310224899efd69e40c78692dddb411b52
Yeonwha <ics22-177@camosun.bc.ca>
2025-05-30 22:03:26 -0700

    defined Food model

--- menuapp-api/menuapp/models.py
+++ menuapp-api/menuapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from django.core.validators import RegexValidator, MinLengthValidator, MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Food(models.Model):
+    category = models.CharField(max_length=20, validators=[
+        RegexValidator('\b(Main?|Dessert?|Drink?)\b')
+    ])
+    name = models.CharField(max_length=30, validators=[
+        RegexValidator('^[A-Za-z0-9 ]+$'),
+        MinLengthValidator(2)
+    ])
+    price = models.FloatField(validators=[
+        MaxValueValidator(999.99),
+        MinValueValidator(0.1)
+    ])
+    checked = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['-id']
