42627f173f29de93f168bbd6fdb36f6ccbcb76de
Yeonwha <ics22-177@camosun.bc.ca>
2025-05-31 22:13:20 -0700

    added restframework

--- menuapp-api/menu_api/settings.py
+++ menuapp-api/menu_api/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 3rd party Apps
+    'rest-framework',
     'menuapp.apps.MenuappConfig',
 ]
 
